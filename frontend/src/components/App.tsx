09016130ec3a5ffe9a0dfcd0a14c2d4166d9749e
Kevin Martins <kev.martinsvieira@gmail.com>
2024-06-18 11:31:02 +0200

    feat: create router

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import SignIn from '../pages/SignIn';
+import LogIn from '../pages/LogIn';
 
 const App = () => {
+  const [user, setUser] = useState(null)
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
